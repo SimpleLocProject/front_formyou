b6fc03dd19fa7810f511a8ebd1390e676ae24cc7
laspargus <maxime.speroni@gmail.com>
2020-06-08 16:33:41 +0200

    design signup page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,67 +1,78 @@
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux'
-import { useHistory } from 'react-router-dom'
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
 
-import { loginSuccess, loginFail } from '../redux/actions/authActions'
-
+import { loginSuccess, loginFail } from "../redux/actions/authActions";
 
 const Login = () => {
-  const isAuthenticated = useSelector(state => state.auth.isAuthenticated)
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const history = useHistory();
 
   const login = async (e) => {
-    e.preventDefault()
-    const API_URL = process.env.REACT_APP_API_URL
+    e.preventDefault();
+    const API_URL = process.env.REACT_APP_API_URL;
     const data = {
       user: {
         email: email,
-        password: password
-      }
-    }
+        password: password,
+      },
+    };
 
     const response = await fetch(`${API_URL}/login`, {
-      method: 'post',
+      method: "post",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(data)
-    })
+      body: JSON.stringify(data),
+    });
 
     try {
-      const token = await response.headers.get('authorization').split(' ')[1]
-      const user = await response.json()
-      const userToLog = { token, user }
-      dispatch(loginSuccess(userToLog))
+      const token = await response.headers.get("authorization").split(" ")[1];
+      const user = await response.json();
+      const userToLog = { token, user };
+      dispatch(loginSuccess(userToLog));
       history.push("/");
     } catch (error) {
-      console.log(error)
-      alert("Aucun utilisateur correspondant")
-      dispatch(loginFail())
+      console.log(error);
+      alert("Aucun utilisateur correspondant");
+      dispatch(loginFail());
     }
-  }
+  };
 
   return (
     <div>
-      {!isAuthenticated &&
+      {!isAuthenticated && (
         <>
           <form onSubmit={login}>
-            <input type="text" placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
-            <input type="password" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+            <input
+              type="text"
+              placeholder="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+            <input
+              type="password"
+              placeholder="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
             <input type="submit" value="Envoyer" />
           </form>
         </>
-      }
-      {isAuthenticated &&
+      )}
+      {isAuthenticated && (
         <>
           <h1>connexion réussi !</h1>
         </>
-      }
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default Login;
