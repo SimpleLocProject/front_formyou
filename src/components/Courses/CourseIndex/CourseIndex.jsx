34833df433ddc8b7d12a6defad778991e40a49f1
laspargus <maxime.speroni@gmail.com>
2020-06-09 16:53:53 +0200

    fetch real course from api and host api function in service

--- src/components/Courses/CourseIndex/CourseIndex.jsx
+++ src/components/Courses/CourseIndex/CourseIndex.jsx
@@ -2,30 +2,16 @@ import React, { useState, useEffect } from "react";
 import CoursePreview from "./../CoursePreview";
 import { fakeCourses } from "./../../../fakecontent/courses";
 import CourseSearch from "./../CourseSearch";
+import { fetchCourse } from "./../../../service/courseApi";
+
 const CourseIndex = () => {
   const [courselist, SetCourseList] = useState();
   const [filteredcourselist, SetFilteredCourseList] = useState();
   const ShortID = require("shortid");
 
   useEffect(() => {
-    // const fetchCourses = async () => {
-    // const API_URL = process.env.REACT_APP_API_URL;
-    // const response = await fetch(`${API_URL}/courses`, {
-    //   method: "get",
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //   },
-    // });
-
-    // try {
-    //   const courses = await response.json();
-    //   SetCourseList(courses);
-    // } catch (error) {
-    //   console.log(error);
-    //   alert("nous ne pouvons pas afficher les cours");
-    // }
-    //}
-    SetCourseList(fakeCourses.courses);
+    const courses = fetchCourse();
+    SetCourseList(courses);
   }, []);
 
   const search = (value) => {
