3841bd65a1fdfa248255e7396952efc430a7d92d
laspargus <maxime.speroni@gmail.com>
2020-06-10 09:51:21 +0200

    add badge on card

--- src/components/Courses/CoursePreview/CoursePreview.jsx
+++ src/components/Courses/CoursePreview/CoursePreview.jsx
@@ -8,6 +8,11 @@ const CoursePreview = ({ course }) => {
         <div className="card-body">
           <h5 className="card-title">{course.title}</h5>
           <p className="card-text">{course.description}</p>
+          {course.categories.map((cat) => (
+            <p>
+              <span class="badge badge-info">{cat.name}</span>
+            </p>
+          ))}
           <Link className="btn btn-primary" to={"/course/" + course.id}>
             Detail
           </Link>
