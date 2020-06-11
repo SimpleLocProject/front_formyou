a3da48c21ec4c88adf0627056af72db28edb2468
nathan-ch <nathanchateau@gmail.com>
2020-06-11 16:29:07 +0200

    Admin can delete a user

--- src/components/Admin/NewUsers.jsx
+++ src/components/Admin/NewUsers.jsx
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { fetchNewUsers } from "./../../service/newUsersApi";
+import { fetchNewUsers, fetchUpdateUser, fetchDeleteUser } from "../../service/userApi";
 import { useDispatch } from "react-redux";
-import { displayError } from "../../redux/middlewares/flashMiddleware";
+import { displayError, displaySuccess } from "../../redux/middlewares/flashMiddleware";
 import { useSelector } from "react-redux";
-import UpdateUser from "./UpdateUser";
 
 const NewUsers = () => {
   const [newUsersList, setNewUsersList] = useState();
@@ -22,14 +21,56 @@ const NewUsers = () => {
       setNewUsersList(newUsers);
     };
     getNewUsers();
-  }, []);
+  }, [dispatch]);
+
+
+  const postUpdateUser = async (token, userId) => {
+    const body = JSON.stringify({
+      user:
+        { can_access: true }
+    })
+    const updateUser = await fetchUpdateUser(token, userId, body);
+    if (!updateUser) {
+      dispatch(displayError("L'utilisateur n'a pas été validé"))
+      return false
+    }
+    if(updateUser){
+      dispatch(displaySuccess("L'utilisateur a bien été validé"))
+    }
+  };
+
+  const postDeleteUser = async (token, userId) => {
+    const deleteUser = await fetchDeleteUser(token, userId);
+    if (!deleteUser) {
+      dispatch(displayError("L'utilisateur n'a pas été supprimé"))
+      return false
+    }
+    if(deleteUser){
+      dispatch(displaySuccess("L'utilisateur a bien été supprimé"))
+    }
+  };
 
   return (
     <>
       <h2>Liste des utilisateurs à modérer</h2>
       {newUsersList ?
        newUsersList.map((newUser)=> (
-       <UpdateUser key={newUser.id} user={newUser}/>
+        <div className="card col-md-4 m-2">
+        <div className="card-body">
+          <h5 className="card-title">{newUser.first_name}</h5>
+          <h5 className="card-title">{newUser.last_name}</h5>
+          <h5 className="card-title">{newUser.email}</h5>
+          <h5 className="card-title">Admin : {newUser.is_admin ? "Oui" : "Non"}</h5>
+          <h5 className="card-title">Professseur : {newUser.is_teacher ? "Oui" : "Non"}</h5>
+          <h5 className="card-title">Utilisateur validé : {newUser.can_access ? "Oui" : "Non"}</h5>
+          <button onClick={() => postUpdateUser(token, newUser.id)} className="btn btn-success m-2">
+            Valider cet utilisateur
+          </button>
+          <button onClick={() => postDeleteUser(token, newUser.id)} className="btn btn-danger m-2">
+            Supprimer cet utilisateur
+          </button>
+        </div>
+      </div>
         )) :
         <p>Aucun utilisateur à modérer</p>}
     </>
